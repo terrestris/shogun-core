8299faf6ca3b73a92009dd91473d7905b41d43bd
Nils Buehner <buehner@terrestris.de>
2014-04-28 17:32:22 +0200

    Added comments to set- and getters of Layer-model.

--- src/shogun2-core/shogun2-model/src/main/java/de/terrestris/shogun2/model/Layer.java
+++ src/shogun2-core/shogun2-model/src/main/java/de/terrestris/shogun2/model/Layer.java
@@ -173,170 +173,317 @@ public abstract class Layer extends PersistentObject {
 	public Layer() {
 	}
 
+	/**
+	 * @return the name
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * @param name
+	 *            the name to set
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
 
+	/**
+	 * @return the projection
+	 */
 	public String getProjection() {
 		return projection;
 	}
 
+	/**
+	 * @param projection
+	 *            the projection to set
+	 */
 	public void setProjection(String projection) {
 		this.projection = projection;
 	}
 
+	/**
+	 * @return the visibility
+	 */
 	public Boolean getVisibility() {
 		return visibility;
 	}
 
+	/**
+	 * @param visibility
+	 *            the visibility to set
+	 */
 	public void setVisibility(Boolean visibility) {
 		this.visibility = visibility;
 	}
 
+	/**
+	 * @return the alwaysInRange
+	 */
 	public Boolean getAlwaysInRange() {
 		return alwaysInRange;
 	}
 
+	/**
+	 * @param alwaysInRange
+	 *            the alwaysInRange to set
+	 */
 	public void setAlwaysInRange(Boolean alwaysInRange) {
 		this.alwaysInRange = alwaysInRange;
 	}
 
+	/**
+	 * @return the displayInLayerSwitcher
+	 */
 	public Boolean getDisplayInLayerSwitcher() {
 		return displayInLayerSwitcher;
 	}
 
+	/**
+	 * @param displayInLayerSwitcher
+	 *            the displayInLayerSwitcher to set
+	 */
 	public void setDisplayInLayerSwitcher(Boolean displayInLayerSwitcher) {
 		this.displayInLayerSwitcher = displayInLayerSwitcher;
 	}
 
+	/**
+	 * @return the opacity
+	 */
 	public Double getOpacity() {
 		return opacity;
 	}
 
+	/**
+	 * @param opacity
+	 *            the opacity to set
+	 */
 	public void setOpacity(Double opacity) {
 		this.opacity = opacity;
 	}
 
+	/**
+	 * @return the attribution
+	 */
 	public String getAttribution() {
 		return attribution;
 	}
 
+	/**
+	 * @param attribution
+	 *            the attribution to set
+	 */
 	public void setAttribution(String attribution) {
 		this.attribution = attribution;
 	}
 
+	/**
+	 * @return the gutter
+	 */
 	public Integer getGutter() {
 		return gutter;
 	}
 
+	/**
+	 * @param gutter
+	 *            the gutter to set
+	 */
 	public void setGutter(Integer gutter) {
 		this.gutter = gutter;
 	}
 
+	/**
+	 * @return the units
+	 */
 	public String getUnits() {
 		return units;
 	}
 
+	/**
+	 * @param units
+	 *            the units to set
+	 */
 	public void setUnits(String units) {
 		this.units = units;
 	}
 
+	/**
+	 * @return the resolutions
+	 */
 	public String getResolutions() {
 		return resolutions;
 	}
 
+	/**
+	 * @param resolutions
+	 *            the resolutions to set
+	 */
 	public void setResolutions(String resolutions) {
 		this.resolutions = resolutions;
 	}
 
+	/**
+	 * @return the scales
+	 */
 	public String getScales() {
 		return scales;
 	}
 
+	/**
+	 * @param scales
+	 *            the scales to set
+	 */
 	public void setScales(String scales) {
 		this.scales = scales;
 	}
 
+	/**
+	 * @return the maxExtent
+	 */
 	public String getMaxExtent() {
 		return maxExtent;
 	}
 
+	/**
+	 * @param maxExtent
+	 *            the maxExtent to set
+	 */
 	public void setMaxExtent(String maxExtent) {
 		this.maxExtent = maxExtent;
 	}
 
+	/**
+	 * @return the minExtent
+	 */
 	public String getMinExtent() {
 		return minExtent;
 	}
 
+	/**
+	 * @param minExtent
+	 *            the minExtent to set
+	 */
 	public void setMinExtent(String minExtent) {
 		this.minExtent = minExtent;
 	}
 
+	/**
+	 * @return the maxResolution
+	 */
 	public Double getMaxResolution() {
 		return maxResolution;
 	}
 
+	/**
+	 * @param maxResolution
+	 *            the maxResolution to set
+	 */
 	public void setMaxResolution(Double maxResolution) {
 		this.maxResolution = maxResolution;
 	}
 
+	/**
+	 * @return the minResolution
+	 */
 	public Double getMinResolution() {
 		return minResolution;
 	}
 
+	/**
+	 * @param minResolution
+	 *            the minResolution to set
+	 */
 	public void setMinResolution(Double minResolution) {
 		this.minResolution = minResolution;
 	}
 
+	/**
+	 * @return the maxScale
+	 */
 	public Double getMaxScale() {
 		return maxScale;
 	}
 
+	/**
+	 * @param maxScale
+	 *            the maxScale to set
+	 */
 	public void setMaxScale(Double maxScale) {
 		this.maxScale = maxScale;
 	}
 
+	/**
+	 * @return the minScale
+	 */
 	public Double getMinScale() {
 		return minScale;
 	}
 
+	/**
+	 * @param minScale
+	 *            the minScale to set
+	 */
 	public void setMinScale(Double minScale) {
 		this.minScale = minScale;
 	}
 
+	/**
+	 * @return the numZoomLevels
+	 */
 	public Integer getNumZoomLevels() {
 		return numZoomLevels;
 	}
 
+	/**
+	 * @param numZoomLevels
+	 *            the numZoomLevels to set
+	 */
 	public void setNumZoomLevels(Integer numZoomLevels) {
 		this.numZoomLevels = numZoomLevels;
 	}
 
+	/**
+	 * @return the displayOutsideMaxExtent
+	 */
 	public Boolean getDisplayOutsideMaxExtent() {
 		return displayOutsideMaxExtent;
 	}
 
+	/**
+	 * @param displayOutsideMaxExtent
+	 *            the displayOutsideMaxExtent to set
+	 */
 	public void setDisplayOutsideMaxExtent(Boolean displayOutsideMaxExtent) {
 		this.displayOutsideMaxExtent = displayOutsideMaxExtent;
 	}
 
+	/**
+	 * @return the isBaseLayer
+	 */
 	public Boolean getIsBaseLayer() {
 		return isBaseLayer;
 	}
 
+	/**
+	 * @param isBaseLayer
+	 *            the isBaseLayer to set
+	 */
 	public void setIsBaseLayer(Boolean isBaseLayer) {
 		this.isBaseLayer = isBaseLayer;
 	}
 
+	/**
+	 * @return the alpha
+	 */
 	public Boolean getAlpha() {
 		return alpha;
 	}
 
+	/**
+	 * @param alpha
+	 *            the alpha to set
+	 */
 	public void setAlpha(Boolean alpha) {
 		this.alpha = alpha;
 	}
