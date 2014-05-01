3c4b1385a98b4aff47417c469909b9b72517a9bf
Nils Bühner <buehner@terrestris.de>
2014-05-02 01:01:08 +0200

    Remove unused import.

--- src/shogun2-core/shogun2-model/src/main/java/de/terrestris/shogun2/model/PersistentObject.java
+++ src/shogun2-core/shogun2-model/src/main/java/de/terrestris/shogun2/model/PersistentObject.java
@@ -10,8 +10,6 @@ import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Inheritance;
-import javax.persistence.InheritanceType;
 import javax.persistence.MappedSuperclass;
 
 import org.apache.commons.lang.builder.EqualsBuilder;
