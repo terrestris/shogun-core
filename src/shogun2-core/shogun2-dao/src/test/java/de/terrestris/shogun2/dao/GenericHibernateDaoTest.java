8b649e990700ac9f447dc19dcc9118eb33a17e09
Nils Bühner <buehner@terrestris.de>
2014-05-12 16:32:48 +0200

    Formatting.

--- src/shogun2-core/shogun2-dao/src/test/java/de/terrestris/shogun2/dao/GenericHibernateDaoTest.java
+++ src/shogun2-core/shogun2-dao/src/test/java/de/terrestris/shogun2/dao/GenericHibernateDaoTest.java
@@ -43,11 +43,11 @@ class ConcreteDao extends GenericHibernateDao<Application, Integer> {
 }
 
 /**
- * This class will test the {@link GenericHibernateDao}. As {@link
- * GenericHibernateDao} is an abstract class, we cannot instantiate it. Instead
- * we will use the {@link ConcreteDao} (as the most basic extension of {@link
- * GenericHibernateDao}) to test the logic contained in {@link
- * GenericHibernateDao}.
+ * This class will test the {@link GenericHibernateDao}. As
+ * {@link GenericHibernateDao} is an abstract class, we cannot instantiate it.
+ * Instead we will use the {@link ConcreteDao} (as the most basic extension of
+ * {@link GenericHibernateDao}) to test the logic contained in
+ * {@link GenericHibernateDao}.
  *
  * @author Marc Jansen
  * @author Nils Bühner
@@ -57,7 +57,6 @@ class ConcreteDao extends GenericHibernateDao<Application, Integer> {
 @Transactional
 @TransactionConfiguration(defaultRollback = true)
 public class GenericHibernateDaoTest {
-
 
 	/**
 	 * We use the {@link ConcreteDao} to test the behaviour of the
@@ -351,8 +350,7 @@ public class GenericHibernateDaoTest {
 	public void findAll_shouldReturnEmptyListWhenNothingPersisted() {
 		List<Application> all = this.dao.findAll();
 
-		assertTrue("findAll() returns list with correct size",
-				all.size() == 0);
+		assertTrue("findAll() returns list with correct size", all.size() == 0);
 	}
 
 	/**
@@ -366,8 +364,7 @@ public class GenericHibernateDaoTest {
 
 		List<Application> all = this.dao.findAll();
 
-		assertTrue("findAll() returns list with correct size",
-				all.size() == 2);
+		assertTrue("findAll() returns list with correct size", all.size() == 2);
 
 		assertTrue("First app is contained when all requested",
 				all.contains(app1));
@@ -376,7 +373,7 @@ public class GenericHibernateDaoTest {
 	}
 
 	/**
-	 *Tests whether findByCriteria() can deal with strange arguments.
+	 * Tests whether findByCriteria() can deal with strange arguments.
 	 */
 	@Test
 	public void findByCriteria_dealsAsExpectedWithVariousArguments() {
@@ -385,20 +382,16 @@ public class GenericHibernateDaoTest {
 		Criterion c = Restrictions.eq("id", 1);
 
 		got = dao.findByCriteria();
-		assertTrue("findByCriteria() doesn't throw when no argument",
-				true);
+		assertTrue("findByCriteria() doesn't throw when no argument", true);
 
 		got = dao.findByCriteria((Criterion) null);
-		assertTrue("findByCriteria() doesn't throw when null argument",
-				true);
+		assertTrue("findByCriteria() doesn't throw when null argument", true);
 
 		got = dao.findByCriteria(c);
-		assertTrue("findByCriteria() doesn't throw when sane argument",
-				true);
+		assertTrue("findByCriteria() doesn't throw when sane argument", true);
 
 		got = dao.findByCriteria(c, (Criterion) null);
-		assertTrue("findByCriteria() doesn't throw when arguments mixed",
-				true);
+		assertTrue("findByCriteria() doesn't throw when arguments mixed", true);
 	}
 
 	/**
