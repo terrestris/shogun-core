03b0613adfd39396915653ee3540719c69ed15b3
Nils Buehner <buehner@terrestris.de>
2014-06-04 15:50:16 +0200

    Add missing person- and user-services.

---
+++ src/shogun2-core/shogun2-service/src/main/java/de/terrestris/shogun2/service/UserService.java
@@ -0,0 +1,17 @@
+package de.terrestris.shogun2.service;
+
+import org.springframework.stereotype.Service;
+
+import de.terrestris.shogun2.model.User;
+
+/**
+ * Service class for the {@link User} model.
+ * 
+ * @author Nils Bühner
+ * @see AbstractCrudService
+ * 
+ */
+@Service("userService")
+public class UserService extends AbstractCrudService<User> {
+
+}
