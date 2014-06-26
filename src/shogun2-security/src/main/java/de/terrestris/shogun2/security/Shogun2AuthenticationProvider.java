0eedba9454717abda4d6674fdc4479d8a648d71a
Nils Buehner <buehner@terrestris.de>
2014-06-26 16:04:22 +0200

    Enhanced security module (including test).
    The SHOGun2 authentication provider will now request a user from the database. The accountName of the user will be used as the principal in the authentication object.

--- src/shogun2-security/src/main/java/de/terrestris/shogun2/security/Shogun2AuthenticationProvider.java
+++ src/shogun2-security/src/main/java/de/terrestris/shogun2/security/Shogun2AuthenticationProvider.java
@@ -1,21 +1,24 @@
 package de.terrestris.shogun2.security;
-/**
- *
- */
-
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import org.apache.log4j.Logger;
+import org.hibernate.criterion.Restrictions;
+import org.hibernate.criterion.SimpleExpression;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.transaction.annotation.Transactional;
+
+import de.terrestris.shogun2.dao.UserDao;
+import de.terrestris.shogun2.model.User;
 
 /**
  * @author Nils Bühner
@@ -29,12 +32,16 @@ public class Shogun2AuthenticationProvider implements AuthenticationProvider {
 	private static final Logger LOG = Logger
 			.getLogger(Shogun2AuthenticationProvider.class);
 
+	@Autowired
+	private UserDao userDao;
+
 	/**
 	 *
 	 * @see org.springframework.security.authentication.AuthenticationProvider#
 	 *      authenticate(org.springframework.security.core.Authentication)
 	 */
 	@Override
+	@Transactional
 	public Authentication authenticate(Authentication authentication)
 			throws AuthenticationException {
 
@@ -44,20 +51,41 @@ public class Shogun2AuthenticationProvider implements AuthenticationProvider {
 		String userName = authentication.getName();
 		String password = (String) authentication.getCredentials();
 
-		// build granted authorities
-		Set<GrantedAuthority> grantedAuthorities = new HashSet<GrantedAuthority>();
-		GrantedAuthority grantedAuthority = new SimpleGrantedAuthority(
-				"ROLE_USER");
-		grantedAuthorities.add(grantedAuthority);
+		// the account name is unique, so we can find at most one user
+		SimpleExpression eqAccountName = Restrictions.eq("accountName",
+				userName);
 
-		// create logged in user object
-		UserDetails userInformation = new User(userName, password, true, true,
-				true, true, grantedAuthorities);
+		List<User> userList = userDao.findByCriteria(eqAccountName);
+		User shogun2User = null;
+
+		// prepare authorities
+		GrantedAuthority userAuthority = new SimpleGrantedAuthority(
+				"ROLE_USER");
+		GrantedAuthority adminAuthority = new SimpleGrantedAuthority(
+				"ROLE_ADMIN");
+		Set<GrantedAuthority> grantedAuthorities = new HashSet<GrantedAuthority>();
+
+		if (userList == null || userList.size() == 0) {
+			throw new AuthenticationCredentialsNotFoundException("Could not find user " + userName);
+		} else {
+			shogun2User = userList.get(0);
+
+			// TODO check password
+			// TODO get authority from user object, i.e. db
+
+			if(userName.equals("admin")){
+				grantedAuthorities.add(adminAuthority);
+			} else if (userName.equals("user")){
+				grantedAuthorities.add(userAuthority);
+			} else {
+				throw new AuthenticationCredentialsNotFoundException("Could not detect authority for user " + userName);
+			}
+		}
 
 		// create corresponding token to forward in Spring Security's filter
 		// chain
 		Authentication authenticationToken = new UsernamePasswordAuthenticationToken(
-				userInformation, authentication.getCredentials(),
+				shogun2User.getAccountName(), shogun2User.getPassword(),
 				grantedAuthorities);
 
 		LOG.info("Finished authentication of the SHOGun2-User");
--- src/shogun2-security/src/test/java/de/terrestris/shogun2/security/Shogun2AuthenticationProviderTest.java
+++ src/shogun2-security/src/test/java/de/terrestris/shogun2/security/Shogun2AuthenticationProviderTest.java
@@ -3,17 +3,35 @@
  */
 package de.terrestris.shogun2.security;
 
-import static org.mockito.Mockito.*;
-import static org.junit.Assert.*;
-import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.Collections;
+import java.util.List;
 
 import org.hamcrest.collection.IsIterableContainingInAnyOrder;
+import org.hibernate.criterion.SimpleExpression;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
+
+import de.terrestris.shogun2.dao.UserDao;
+import de.terrestris.shogun2.model.User;
 
 /**
  * @author Nils Bühner
@@ -21,19 +39,44 @@ import org.springframework.security.core.userdetails.UserDetails;
  */
 public class Shogun2AuthenticationProviderTest {
 
+	@Mock
+	private UserDao userDao;
+
+	@InjectMocks
+	private Shogun2AuthenticationProvider authProvider;
+
+	@Before
+	public void setUp() {
+		// Process mock annotations
+		MockitoAnnotations.initMocks(this);
+	}
+
 	@Test
 	public void authenticate_shouldAssignRoleUser() {
 
 		// 1. Mock an authentication request object
-		String shogun2UserName = "Some SHOGun2 User";
-		String shogun2UserPass = "SomePassword";
+		final String shogun2UserName = "user";
+		final String shogun2UserPass = "user";
 
 		Authentication authRequest = mock(Authentication.class);
 		when(authRequest.getName()).thenReturn(shogun2UserName);
 		when(authRequest.getCredentials()).thenReturn(shogun2UserPass);
 
+		// 2. Mock the userDao
+		doAnswer(new Answer<List<User>>() {
+
+			public List<User> answer(InvocationOnMock invocation)
+					throws NoSuchFieldException, SecurityException,
+					IllegalArgumentException, IllegalAccessException {
+
+				User u = new User("firstName", "lastName", shogun2UserName, shogun2UserPass);
+
+				return Collections.singletonList(u);
+			}
+		}).when(userDao).findByCriteria(any(SimpleExpression.class));
+
 		// 2. Call the authenticate method with the mocked object
-		Shogun2AuthenticationProvider authProvider = new Shogun2AuthenticationProvider();
+//		Shogun2AuthenticationProvider authProvider = new Shogun2AuthenticationProvider();
 		Authentication authResult = authProvider.authenticate(authRequest);
 
 		// 3. Assert that the authResult is valid (e.g. that ROLE_USER has been
@@ -47,8 +90,8 @@ public class Shogun2AuthenticationProviderTest {
 				instanceOf(UsernamePasswordAuthenticationToken.class));
 		assertTrue(authResult.isAuthenticated());
 
-		assertThat(authResult.getPrincipal(), instanceOf(UserDetails.class));
-		assertEquals(shogun2UserName, ((UserDetails) authResult.getPrincipal()).getUsername());
+		assertThat(authResult.getPrincipal(), instanceOf(String.class));
+		assertEquals(shogun2UserName, authResult.getPrincipal());
 		assertEquals(shogun2UserPass, authResult.getCredentials());
 
 		// thx to http://stackoverflow.com/a/12167781
