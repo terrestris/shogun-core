7ba9092844bb371d213210d94e010816891c0876
Kai Volland <kaivolland@yahoo.de>
2015-10-28 17:49:10 +0000

    Introduces the Image module

---
+++ src/shogun2-core/shogun2-model/src/main/java/de/terrestris/shogun2/model/module/Image.java
@@ -0,0 +1,130 @@
+/**
+ * 
+ */
+package de.terrestris.shogun2.model.module;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+/**
+ * A LayerTree is a simple module, where layers (of a map) are organized in a
+ * flexible tree structure.
+ * 
+ * @author Nils Bühner
+ *
+ */
+@Entity
+@Table
+public class Image extends Module {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Explicitly adding the default constructor as this is important, e.g. for
+	 * Hibernate: http://goo.gl/3Cr1pw
+	 */
+	public Image() {
+	}
+	
+	/**
+	 * The path to the image;
+	 */
+	private String src;
+	
+	/**
+	 * An optional link, if the image is clickable.
+	 */
+	private String link;
+	
+	/**
+	 * The alternative Text for the Image.
+	 */
+	private String altText;
+
+	/**
+	 * @return the src
+	 */
+	public String getSrc() {
+		return src;
+	}
+
+	/**
+	 * @param src the src to set
+	 */
+	public void setSrc(String src) {
+		this.src = src;
+	}
+
+	/**
+	 * @return the link
+	 */
+	public String getLink() {
+		return link;
+	}
+
+	/**
+	 * @param link the link to set
+	 */
+	public void setLink(String link) {
+		this.link = link;
+	}
+
+	/**
+	 * @return the altText
+	 */
+	public String getAltText() {
+		return altText;
+	}
+
+	/**
+	 * @param altText the altText to set
+	 */
+	public void setAltText(String altText) {
+		this.altText = altText;
+	}
+
+	/**
+	 * @see java.lang.Object#hashCode()
+	 *
+	 *      According to
+	 *      http://stackoverflow.com/questions/27581/overriding-equals
+	 *      -and-hashcode-in-java it is recommended only to use getter-methods
+	 *      when using ORM like Hibernate
+	 */
+	public int hashCode() {
+		// two randomly chosen prime numbers
+		return new HashCodeBuilder(7, 3).appendSuper(super.hashCode()).toHashCode();
+	}
+
+	/**
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 *
+	 *      According to
+	 *      http://stackoverflow.com/questions/27581/overriding-equals
+	 *      -and-hashcode-in-java it is recommended only to use getter-methods
+	 *      when using ORM like Hibernate
+	 */
+	public boolean equals(Object obj) {
+		if (!(obj instanceof Image))
+			return false;
+		Image other = (Image) obj;
+
+		return new EqualsBuilder().appendSuper(super.equals(other)).isEquals();
+	}
+
+	/**
+	 *
+	 */
+	public String toString() {
+		return new ToStringBuilder(this, ToStringStyle.DEFAULT_STYLE).appendSuper(super.toString()).toString();
+	}
+
+}
