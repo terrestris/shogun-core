e24e96364f44f0e1a6bc3f3318031b59a3012c93
Kai Volland <kaivolland@yahoo.de>
2015-10-28 18:35:33 +0000

    Introduces the Print module

---
+++ src/shogun2-core/shogun2-model/src/main/java/de/terrestris/shogun2/model/module/Print.java
@@ -0,0 +1,92 @@
+/**
+ * 
+ */
+package de.terrestris.shogun2.model.module;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+/**
+ * A LayerTree is a simple module, where layers (of a map) are organized in a
+ * flexible tree structure.
+ * 
+ * @author Nils Bühner
+ *
+ */
+@Entity
+@Table
+public class Print extends Module {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Explicitly adding the default constructor as this is important, e.g. for
+	 * Hibernate: http://goo.gl/3Cr1pw
+	 */
+	public Print() {
+	}
+	
+	/**
+	 * The mapfish-print url.
+	 */
+	private String url;
+
+	/**
+	 * @return the url
+	 */
+	public String getUrl() {
+		return url;
+	}
+
+	/**
+	 * @param url the url to set
+	 */
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	/**
+	 * @see java.lang.Object#hashCode()
+	 *
+	 *      According to
+	 *      http://stackoverflow.com/questions/27581/overriding-equals
+	 *      -and-hashcode-in-java it is recommended only to use getter-methods
+	 *      when using ORM like Hibernate
+	 */
+	public int hashCode() {
+		// two randomly chosen prime numbers
+		return new HashCodeBuilder(23, 3).appendSuper(super.hashCode()).toHashCode();
+	}
+
+	/**
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 *
+	 *      According to
+	 *      http://stackoverflow.com/questions/27581/overriding-equals
+	 *      -and-hashcode-in-java it is recommended only to use getter-methods
+	 *      when using ORM like Hibernate
+	 */
+	public boolean equals(Object obj) {
+		if (!(obj instanceof Print))
+			return false;
+		Print other = (Print) obj;
+
+		return new EqualsBuilder().appendSuper(super.equals(other)).isEquals();
+	}
+
+	/**
+	 *
+	 */
+	public String toString() {
+		return new ToStringBuilder(this, ToStringStyle.DEFAULT_STYLE).appendSuper(super.toString()).toString();
+	}
+
+}
