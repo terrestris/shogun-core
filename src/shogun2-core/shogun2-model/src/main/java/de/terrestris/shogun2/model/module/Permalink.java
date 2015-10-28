a7d3a3a31584b6bc12c6d9721784f7b91be485f8
Kai Volland <kaivolland@yahoo.de>
2015-10-28 18:15:32 +0000

    Introduces the Permalink module

---
+++ src/shogun2-core/shogun2-model/src/main/java/de/terrestris/shogun2/model/module/Permalink.java
@@ -0,0 +1,73 @@
+/**
+ * 
+ */
+package de.terrestris.shogun2.model.module;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+/**
+ * A LayerTree is a simple module, where layers (of a map) are organized in a
+ * flexible tree structure.
+ * 
+ * @author Nils Bühner
+ *
+ */
+@Entity
+@Table
+public class Permalink extends Module {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Explicitly adding the default constructor as this is important, e.g. for
+	 * Hibernate: http://goo.gl/3Cr1pw
+	 */
+	public Permalink() {
+	}
+
+	/**
+	 * @see java.lang.Object#hashCode()
+	 *
+	 *      According to
+	 *      http://stackoverflow.com/questions/27581/overriding-equals
+	 *      -and-hashcode-in-java it is recommended only to use getter-methods
+	 *      when using ORM like Hibernate
+	 */
+	public int hashCode() {
+		// two randomly chosen prime numbers
+		return new HashCodeBuilder(13, 3).appendSuper(super.hashCode()).toHashCode();
+	}
+
+	/**
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 *
+	 *      According to
+	 *      http://stackoverflow.com/questions/27581/overriding-equals
+	 *      -and-hashcode-in-java it is recommended only to use getter-methods
+	 *      when using ORM like Hibernate
+	 */
+	public boolean equals(Object obj) {
+		if (!(obj instanceof Permalink))
+			return false;
+		Permalink other = (Permalink) obj;
+
+		return new EqualsBuilder().appendSuper(super.equals(other)).isEquals();
+	}
+
+	/**
+	 *
+	 */
+	public String toString() {
+		return new ToStringBuilder(this, ToStringStyle.DEFAULT_STYLE).appendSuper(super.toString()).toString();
+	}
+
+}
