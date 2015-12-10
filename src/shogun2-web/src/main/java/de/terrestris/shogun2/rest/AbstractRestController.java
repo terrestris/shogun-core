e4184dc6f9371e7ce6060b335900c0a2ce47c9af
Kai Volland <kaivolland@yahoo.de>
2015-12-10 10:11:45 +0100

    Improves error handling and HTTP-Status Codes

--- src/shogun2-web/src/main/java/de/terrestris/shogun2/rest/AbstractRestController.java
+++ src/shogun2-web/src/main/java/de/terrestris/shogun2/rest/AbstractRestController.java
@@ -19,79 +19,100 @@ import de.terrestris.shogun2.service.AbstractCrudService;
  * @author Daniel Koch
  *
  */
-@RequestMapping(value="/rest")
+@RequestMapping(value = "/rest")
 public abstract class AbstractRestController<E extends PersistentObject> {
 
 	/**
 	 * The Logger
 	 */
-	private static final Logger LOG = Logger.getLogger(AbstractRestController.class);
+	private static final Logger LOG = Logger
+			.getLogger(AbstractRestController.class);
 
 	@Autowired
 	private AbstractCrudService<E> service;
 
 	/**
 	 * Find all Entities.
+	 *
 	 * @return
 	 */
 	@RequestMapping(method = RequestMethod.GET)
 	public ResponseEntity<List<E>> findAll() {
-		return new ResponseEntity<List<E>>(this.service.findAll(), HttpStatus.OK);
-	}
-
-	/**
-	 * Create an entity.
-	 * @param entity
-	 * @return
-	 */
-	@RequestMapping(method=RequestMethod.POST)
-	public ResponseEntity<E> create(@RequestBody E entity) {
-		LOG.debug("create() with body "+entity+" of type " + entity.getClass());
-		E created = this.service.saveOrUpdate(entity);
-		return new ResponseEntity<E>(created, HttpStatus.OK);
+		return new ResponseEntity<List<E>>(this.service.findAll(),
+				HttpStatus.OK);
 	}
 
 	/**
 	 * Get an entity by id.
+	 *
 	 * @param id
 	 * @return
 	 */
-	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<E> get(@PathVariable int id) {
 		E entity = this.service.findById(id);
 		return new ResponseEntity<E>(entity, HttpStatus.OK);
 	}
 
 	/**
-	 * Updates an entity by id.
-	 * @param id
+	 * Create an entity.
+	 *
 	 * @param entity
 	 * @return
-	 * @throws Exception
 	 */
-	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
-	public ResponseEntity<E> update(@PathVariable int id, @RequestBody E entity) {
-		LOG.debug("update " + entity.getClass() + id +" with body" + entity);
-
-		if (entity.getId() == id) {
-			E updated = this.service.saveOrUpdate(entity);
-			LOG.debug("updated entity: " + updated);
-			return new ResponseEntity<E>(updated, HttpStatus.OK);
-		} else {
-			return new ResponseEntity<E>(HttpStatus.NOT_MODIFIED);
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<E> create(@RequestBody E entity) {
+		try {
+			E created = this.service.saveOrUpdate(entity);
+			LOG.debug("Create " + entity.getClass() + " with body " + entity);
+			return new ResponseEntity<E>(created, HttpStatus.CREATED);
+		} catch (Exception e) {
+			LOG.debug("Malformed body: " + entity);
+			return new ResponseEntity<E>(HttpStatus.BAD_REQUEST);
 		}
 	}
 
 	/**
+	 * Updates an entity by id.
+	 *
+	 * @param id
+	 * @param entity
+	 * @return
+	 */
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<E> update(@PathVariable int id, @RequestBody E entity) {
+		if (entity.getId() == id) {
+			try {
+				E updated = this.service.saveOrUpdate(entity);
+				LOG.debug("Updated entity: " + updated);
+				return new ResponseEntity<E>(updated, HttpStatus.OK);
+			} catch (Exception e) {
+				LOG.error("There is no " + entity.getClass() + " with id " + id);
+				return new ResponseEntity<E>(HttpStatus.NOT_FOUND);
+			}
+		} else {
+			LOG.error("Can't update " + id + " with body" + entity);
+			return new ResponseEntity<E>(HttpStatus.BAD_REQUEST);
+		}
+	}
+
+	/**
+	 * Deletes an entity by id.
 	 *
 	 * @param id
 	 * @return
 	 */
-	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
 	public ResponseEntity<E> delete(@PathVariable int id) {
 		E deleted = this.service.findById(id);
-		this.service.delete(deleted);
-		return new ResponseEntity<E>(deleted, HttpStatus.OK);
+		if(deleted != null) {
+			this.service.delete(deleted);
+			LOG.debug("Delted " + id + " of type " + deleted.getClass());
+			return new ResponseEntity<E>(deleted, HttpStatus.OK);
+		} else {
+			LOG.error("There is no object with id " + id);
+			return new ResponseEntity<E>(HttpStatus.NOT_FOUND);
+		}
 	}
 
 }
