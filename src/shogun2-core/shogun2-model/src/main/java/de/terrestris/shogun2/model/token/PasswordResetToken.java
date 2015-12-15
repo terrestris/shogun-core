3b5cfdd171066d302537de59ef902a7e7291fa1b
Daniel Koch <koch@terrestris.de>
2015-12-15 10:42:31 +0100

    Add comment block

--- src/shogun2-core/shogun2-model/src/main/java/de/terrestris/shogun2/model/token/PasswordResetToken.java
+++ src/shogun2-core/shogun2-model/src/main/java/de/terrestris/shogun2/model/token/PasswordResetToken.java
@@ -8,7 +8,13 @@ import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 
-
+/**
+ *
+ * The PasswordResetToken class.
+ *
+ * @author Daniel Koch
+ *
+ */
 @Entity
 @Table
 public class PasswordResetToken extends Token {
