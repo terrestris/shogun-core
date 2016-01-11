aba93ae13f352916ab5287c52cb0a2e1039dc545
Daniel Koch <koch@terrestris.de>
2016-01-11 11:30:02 +0100

    Prepare shogun2 for automated release process:   - Add maven-release plugin   - Add missing Central Repository metadata   - Add release profile   - Add release scripts (full and snapshot)

---
+++ scripts/release.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# stop at first command failure
+set -e
+
+read -p "Do you really want to deploy to maven central repository (yes/no)? "
+
+# check if prompted to continue
+if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+    exit 1
+fi
+
+# check if the input parameter RELEASE_VERSION is valid
+RELEASE_VERSION="$1"
+if [[ ! $RELEASE_VERSION =~ ^([0-9]+\.[0-9]+\.[0-9])(\-SNAPSHOT){0,1}$ ]]; then
+    echo "Error: RELEASE_VERSION must be in X.Y.Z(-SNAPSHOT) format, but was $RELEASE_VERSION"
+    exit 1
+fi
+
+mvn release:clean
+mvn release:prepare --batch-mode -DreleaseVersion=$RELEASE_VERSION
+mvn release:perform --batch-mode
---
+++ scripts/snapshot-release.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# stop at first command failure
+set -e
+
+if [ "$TRAVIS" != "true" ]; then
+    echo "This script is supposed to be run inside the travis environment."
+    exit 1
+fi
+
+if [ $TRAVIS_PULL_REQUEST != "false" ]; then
+    # only proceed if we were called by a merge (and not by a PR request)
+    exit 1
+fi
+
+if [ $TRAVIS_BRANCH != "master" ]; then
+    # only proceed if the target branch is master
+    exit 1
+fi
+
+if [ "$TRAVIS_JDK_VERSION" != "openjdk7" ]; then
+    # only proceed if the target JDK is openjdk7
+    exit 1
+fi
+
+SCRIPTDIR=`dirname "$0"`
+
+SETTINGSFILE=$SCRIPTDIR/settings.xml
+
+mvn clean deploy --settings $SETTINGSFILE
