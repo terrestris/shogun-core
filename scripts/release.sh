9a1d80969964e68a7880b991cbfea380af59f368
Nils Bühner <buehner@terrestris.de>
2016-01-14 11:39:53 +0100

    Adapt release script to capture DEV version

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,7 +3,7 @@
 # stop at first command failure
 set -e
 
-read -p "Do you really want to deploy to maven central repository (yes/no)? "
+read -p "Do you really want to deploy to maven central repository (y/n)? "
 
 # check if prompted to continue
 if [[ ! $REPLY =~ ^[Yy]$ ]]; then
@@ -12,8 +12,15 @@ fi
 
 # check if the input parameter RELEASE_VERSION is valid
 RELEASE_VERSION="$1"
-if [[ ! $RELEASE_VERSION =~ ^([0-9]+\.[0-9]+\.[0-9])(\-SNAPSHOT){0,1}$ ]]; then
-    echo "Error: RELEASE_VERSION must be in X.Y.Z(-SNAPSHOT) format, but was $RELEASE_VERSION"
+if [[ ! $RELEASE_VERSION =~ ^([0-9]+\.[0-9]+\.[0-9])$ ]]; then
+    echo "Error: RELEASE_VERSION must be in X.Y.Z format, but was $RELEASE_VERSION"
+    exit 1
+fi
+
+# check if the input parameter DEVELOPMENT_VERSION is valid
+DEVELOPMENT_VERSION="$2"
+if [[ ! $DEVELOPMENT_VERSION =~ ^([0-9]+\.[0-9]+\.[0-9])(\-SNAPSHOT)$ ]]; then
+    echo "Error: DEVELOPMENT_VERSION must be in X.Y.Z-SNAPSHOT format, but was $DEVELOPMENT_VERSION"
     exit 1
 fi
 
@@ -22,8 +29,7 @@ SCRIPTDIR=`dirname "$0"`
 pushd $SCRIPTDIR/../src/
 
 mvn release:clean
-mvn release:prepare --batch-mode -DreleaseVersion=$RELEASE_VERSION
+mvn release:prepare --batch-mode -DreleaseVersion=$RELEASE_VERSION -DdevelopmentVersion=$DEVELOPMENT_VERSION -Darguments="-DskipTests"
 mvn release:perform --batch-mode
 
 popd
-
