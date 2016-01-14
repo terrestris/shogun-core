47caed190928d998d581eca6f525657cd32f4353
Nils Bühner <buehner@terrestris.de>
2016-01-14 16:09:47 +0100

    Refactor: Move methods to new util

---
+++ src/shogun2-util/src/main/java/de/terrestris/shogun2/util/data/ResultSet.java
@@ -0,0 +1,56 @@
+package de.terrestris.shogun2.util.data;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Nils Bühner
+ *
+ */
+public final class ResultSet {
+
+	/**
+	 *
+	 * @param data
+	 * @return
+	 */
+	public static final Map<String, Object> success(Collection<? extends Object> data) {
+
+		Map<String, Object> returnMap = new HashMap<String, Object>(3);
+		returnMap.put("total", data.size());
+		returnMap.put("data", data);
+		returnMap.put("success", true);
+
+		return returnMap;
+	}
+
+	/**
+	 *
+	 * @param dataset
+	 * @return
+	 */
+	public static final Map<String, Object> success(Object dataset) {
+
+		Map<String, Object> returnMap = new HashMap<String, Object>(3);
+		returnMap.put("total", 1);
+		returnMap.put("data", dataset);
+		returnMap.put("success", true);
+
+		return returnMap;
+	}
+
+	/**
+	 *
+	 * @param msg
+	 * @return
+	 */
+	public static final Map<String, Object> error(String msg) {
+
+		Map<String, Object> returnMap = new HashMap<String, Object>(2);
+		returnMap.put("message", msg);
+		returnMap.put("success", false);
+
+		return returnMap;
+	}
+}
