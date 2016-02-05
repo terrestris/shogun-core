64783b9a0247c16c73247fce794056a1e6228ed5
Nils Bühner <buehner@terrestris.de>
2016-02-05 17:30:54 +0100

    Remove uncommented code

--- src/shogun2-core/shogun2-model/src/main/java/de/terrestris/shogun2/model/SecuredPersistentObject.java
+++ src/shogun2-core/shogun2-model/src/main/java/de/terrestris/shogun2/model/SecuredPersistentObject.java
@@ -26,19 +26,13 @@ public abstract class SecuredPersistentObject extends PersistentObject {
 	private static final long serialVersionUID = 1L;
 
 	@OneToMany
-//	@CollectionTable(name = "ENTITY_USERPERMISSIONS", joinColumns = @JoinColumn(name = "ENTITY_ID"))
-//	@JoinTable(joinColumns = @JoinColumn(name = "ENTITY_ID"))
 	@Cascade(CascadeType.ALL)
 	@JsonIgnore
-//	@MapKeyJoinColumn(name="USER_ID")
 	private Map<User, PermissionCollection> userPermissions = new HashMap<User, PermissionCollection>();
 
 	@OneToMany
-//	@CollectionTable(name = "ENTITY_GROUPPERMISSIONS", joinColumns = @JoinColumn(name = "ENTITY_ID"))
-//	@JoinTable(joinColumns = @JoinColumn(name = "ENTITY_ID"))
 	@Cascade(CascadeType.ALL)
 	@JsonIgnore
-//	@MapKeyJoinColumn(name="GROUP_ID")
 	private Map<UserGroup, PermissionCollection> groupPermissions = new HashMap<UserGroup, PermissionCollection>();
 
 	/**
