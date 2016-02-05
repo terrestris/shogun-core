c71136411ec7bdac7dd8d5d64a33cf43fa773e9c
Nils Bühner <buehner@terrestris.de>
2016-02-05 17:09:26 +0100

    Adapt archetype to changes in base library

--- src/shogun2-webapp-archetype/src/main/resources/archetype-resources/src/main/resources/META-INF/sql/cleanup_acl_db.sql
+++
@@ -1,7 +0,0 @@
-#set( $symbol_pound = '#' )
-#set( $symbol_dollar = '$' )
-#set( $symbol_escape = '\' )
-delete from ACL_ENTRY;
-delete from ACL_OBJECT_IDENTITY;
-delete from ACL_SID;
-delete from ACL_CLASS;
