e352e6b68be555c351c9622d5336c774d5a47c54
Nils Bühner <buehner@terrestris.de>
2016-04-08 17:19:26 +0200

    Load entities from the DB when resolving by IDs

--- src/shogun2-core/src/main/java/de/terrestris/shogun2/converter/PersistentObjectIdResolver.java
+++ src/shogun2-core/src/main/java/de/terrestris/shogun2/converter/PersistentObjectIdResolver.java
@@ -1,50 +1,75 @@
 package de.terrestris.shogun2.converter;
 
 import org.apache.log4j.Logger;
+import org.springframework.web.context.support.SpringBeanAutowiringSupport;
 
 import com.fasterxml.jackson.annotation.ObjectIdGenerator.IdKey;
 import com.fasterxml.jackson.annotation.ObjectIdResolver;
 import com.fasterxml.jackson.annotation.SimpleObjectIdResolver;
 
-import de.terrestris.shogun2.helper.IdHelper;
+import de.terrestris.shogun2.dao.GenericHibernateDao;
 import de.terrestris.shogun2.model.PersistentObject;
+import de.terrestris.shogun2.service.AbstractCrudService;
 
 /**
  *
- * An ID resolver for {@link PersistentObject}s when deserializing only on the base
- * of ID values. Extends the default implementation.
+ * An ID resolver for {@link PersistentObject}s when deserializing only on the
+ * base of ID values. Based on a given ID, this resolver will load the whole
+ * entity from the database. Extends the default implementation.
  *
  * @author Nils Buehner
  *
  */
-public abstract class PersistentObjectIdResolver<E extends PersistentObject> extends SimpleObjectIdResolver {
+public abstract class PersistentObjectIdResolver<E extends PersistentObject, D extends GenericHibernateDao<E, Integer>, S extends AbstractCrudService<E, D>>
+		extends SimpleObjectIdResolver {
 
 	protected final Logger LOG = Logger.getLogger(getClass());
 
-	protected final Class<E> entityClass;
+	protected S service;
 
 	/**
-	 * Constructor that has to be called by subclasses.
+	 * Default Constructor that injects beans automatically.
 	 *
 	 * @param entityClass
 	 */
-	protected PersistentObjectIdResolver(Class<E> entityClass) {
-		this.entityClass = entityClass;
+	protected PersistentObjectIdResolver() {
+		// As subclasses of this class are used in the resolver property of an
+		// JsonIdentityInfo annotation, we cannot easily autowire components
+		// (like the service for the current). For that reason, we use this
+		// helper method to process the injection of the services
+		SpringBeanAutowiringSupport.processInjectionBasedOnCurrentContext(this);
 	}
 
+	/**
+	 * Has to be implemented by subclasses to autowire and set the correct
+	 * service class.
+	 *
+	 * @param service
+	 *            the service to set
+	 */
+	public abstract void setService(S service);
+
+	/**
+	 *
+	 */
 	@Override
 	public void bindItem(IdKey id, Object ob) {
 		super.bindItem(id, ob);
 	}
 
+	/**
+	 *
+	 */
 	@Override
-	public E resolveId(IdKey id) {
+	public E resolveId(IdKey idKey) {
 		try {
-			if (id.key instanceof Integer) {
-				// return a "dummy" object, which only has the correct ID value
-				E entity = getEntityClass().newInstance();
-				IdHelper.setIdOnPersistentObject(entity, (Integer) id.key);
-				return entity;
+			if (idKey.key instanceof Integer) {
+				final Integer id = (Integer) idKey.key;
+				// we only "load" the entity to follow a lazy approach, i.e.
+				// requests to the database will only be queried if any properties
+				// of the entity will (later) be accessed (which may already
+				// the case when jackson is doing some magic)
+				return service.loadById(id);
 			} else {
 				throw new Exception("ID is not of type Integer.");
 			}
@@ -55,11 +80,17 @@ public abstract class PersistentObjectIdResolver<E extends PersistentObject> ext
 
 	}
 
+	/**
+	 *
+	 */
 	@Override
 	public boolean canUseFor(ObjectIdResolver resolverType) {
 		return super.canUseFor(resolverType);
 	}
 
+	/**
+	 *
+	 */
 	@Override
 	public ObjectIdResolver newForDeserialization(Object context) {
 		try {
@@ -71,9 +102,9 @@ public abstract class PersistentObjectIdResolver<E extends PersistentObject> ext
 	}
 
 	/**
-	 * @return the entityClass
+	 * @return the service
 	 */
-	public Class<E> getEntityClass() {
-		return entityClass;
+	public S getService() {
+		return service;
 	}
 }
