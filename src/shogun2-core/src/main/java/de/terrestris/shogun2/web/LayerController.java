61284712062b634449500d61be07375b64da432a
Nils Bühner <buehner@terrestris.de>
2016-04-22 13:58:58 +0200

    Refactor REST/web tier of Layer hierarchy

---
+++ src/shogun2-core/src/main/java/de/terrestris/shogun2/web/LayerController.java
@@ -0,0 +1,55 @@
+/**
+ *
+ */
+package de.terrestris.shogun2.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import de.terrestris.shogun2.dao.LayerDao;
+import de.terrestris.shogun2.model.layer.Layer;
+import de.terrestris.shogun2.service.LayerService;
+
+/**
+ *
+ * @author Johannes Weskamm
+ * @author Kai Volland
+ * @author Nils Bühner
+ *
+ */
+@Controller
+@RequestMapping("/layer")
+public class LayerController<E extends Layer, D extends LayerDao<E>, S extends LayerService<E, D>>
+		extends AbstractLayerController<E, D, S> {
+
+	/**
+	 * Default constructor, which calls the type-constructor
+	 */
+	@SuppressWarnings("unchecked")
+	public LayerController() {
+		this((Class<E>) Layer.class);
+	}
+
+	/**
+	 * Constructor that sets the concrete entity class for the controller.
+	 * Subclasses MUST call this constructor.
+	 */
+	protected LayerController(Class<E> entityClass) {
+		super(entityClass);
+	}
+
+	/**
+	 * We have to use {@link Qualifier} to define the correct service here.
+	 * Otherwise, spring can not decide which service has to be autowired here
+	 * as there are multiple candidates.
+	 */
+	@Override
+	@Autowired
+	@Qualifier("layerService")
+	public void setService(S service) {
+		this.service = service;
+	}
+
+}
