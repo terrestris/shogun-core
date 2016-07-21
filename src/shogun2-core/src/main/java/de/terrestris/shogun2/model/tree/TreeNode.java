993f711c419628ad430831c574ed6d22f5950017
Nils Bühner <buehner@terrestris.de>
2016-07-21 18:52:56 +0200

    Add class information to enhance deserializing

--- src/shogun2-core/src/main/java/de/terrestris/shogun2/model/tree/TreeNode.java
+++ src/shogun2-core/src/main/java/de/terrestris/shogun2/model/tree/TreeNode.java
@@ -20,6 +20,7 @@ import com.fasterxml.jackson.annotation.JsonIdentityReference;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 
 import de.terrestris.shogun2.converter.TreeFolderIdResolver;
@@ -39,6 +40,7 @@ import de.terrestris.shogun2.model.PersistentObject;
 @Table
 @Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 @JsonInclude(Include.NON_NULL)
+@JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, include = JsonTypeInfo.As.PROPERTY, property = "@class")
 public class TreeNode extends PersistentObject {
 
 	/**
