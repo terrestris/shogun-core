fb85a22edf8437a0a29dcc3dbfd65591389a1170
Nils Bühner <buehner@terrestris.de>
2016-10-28 14:11:23 +0200

    Extend AbstractCrudService by simple filter method

--- src/shogun2-core/src/main/java/de/terrestris/shogun2/service/AbstractCrudService.java
+++ src/shogun2-core/src/main/java/de/terrestris/shogun2/service/AbstractCrudService.java
@@ -1,11 +1,18 @@
 package de.terrestris.shogun2.service;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
+import org.hibernate.criterion.Conjunction;
+import org.hibernate.criterion.Criterion;
+import org.hibernate.criterion.Disjunction;
+import org.hibernate.criterion.Restrictions;
+import org.hibernate.criterion.SimpleExpression;
 import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.access.prepost.PostFilter;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.util.MultiValueMap;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
@@ -13,6 +20,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import de.terrestris.shogun2.dao.GenericHibernateDao;
 import de.terrestris.shogun2.model.PersistentObject;
+import de.terrestris.shogun2.util.entity.EntityUtil;
 
 /**
  * This abstract service class provides basic CRUD functionality.
@@ -91,6 +99,75 @@ public abstract class AbstractCrudService<E extends PersistentObject, D extends 
 	@PostFilter("hasRole(@configHolder.getSuperAdminRoleName()) or hasPermission(filterObject, 'READ')")
 	public List<E> findAll() {
 		return dao.findAll();
+	}
+
+	/**
+	 * Finds all entities that match the given filter (multi value map).
+	 *
+	 * Each key in the multi value map is the name of a field/property of the
+	 * entity. These values may be case insensitive! Each field name is mapped
+	 * to a list of values (passed as Strings).
+	 *
+	 * Fields that do not exist in the entity will be ignored.
+	 *
+	 * Example:
+	 *
+	 * int=['1','2']
+	 * string=['foo']
+	 * bool1=['0']
+	 * bool2=['true']
+	 *
+	 * would be translated to a filter like (values are casted to the target type of the field)
+	 *
+	 * (int == 1 || int == 2) && (string == 'foo') && (bool1 == false) && (bool2 == true)
+	 *
+	 * and return all entities that match this condition.
+	 *
+	 * This will only work on simple properties of an entity.
+	 *
+	 * @param requestedFilter
+	 * @return
+	 */
+	@PostFilter("hasRole(@configHolder.getSuperAdminRoleName()) or hasPermission(filterObject, 'READ')")
+	public List<E> findBySimpleFilter(MultiValueMap<String,String> requestedFilter) {
+
+		MultiValueMap<String, Object> origFieldNamesToCastedValues = EntityUtil
+				.validFieldNamesWithCastedValues(requestedFilter, getEntityClass());
+
+		// start with an empty list
+		List<E> results = new ArrayList<>();
+
+		List<Criterion> orPredicates = new ArrayList<>();
+
+		if(origFieldNamesToCastedValues != null && !origFieldNamesToCastedValues.isEmpty()) {
+
+			for (String fieldName : origFieldNamesToCastedValues.keySet()) {
+				List<Object> fieldValues = origFieldNamesToCastedValues.get(fieldName);
+
+				// if there are multiple values for a field name, we'll check
+				// for equality and connect them with OR
+				List<Criterion> eqExpressions = new ArrayList<>();
+
+				for (Object fieldValue : fieldValues) {
+					final SimpleExpression eq = Restrictions.eq(fieldName, fieldValue);
+					eqExpressions.add(eq);
+				}
+
+				if(!eqExpressions.isEmpty()) {
+					final Criterion[] eqArray = eqExpressions.toArray(new Criterion[0]);
+					final Disjunction or = Restrictions.or(eqArray);
+					orPredicates.add(or);
+				}
+			}
+
+			if(!orPredicates.isEmpty()) {
+				final Criterion[] orArray = orPredicates.toArray(new Criterion[0]);
+				final Conjunction and = Restrictions.and(orArray);
+				results = dao.findByCriteria(and);
+			}
+		}
+
+		return results;
 	}
 
 	/**
