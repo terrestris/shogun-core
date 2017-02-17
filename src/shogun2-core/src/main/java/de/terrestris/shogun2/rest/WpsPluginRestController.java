fad4bac50b19f880a55cb867f34627df61df75fa
Nils Bühner <buehner@terrestris.de>
2017-02-17 10:32:45 +0100

    Add WpsPlugin model/dao/service/controllers

---
+++ src/shogun2-core/src/main/java/de/terrestris/shogun2/rest/WpsPluginRestController.java
@@ -0,0 +1,48 @@
+package de.terrestris.shogun2.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import de.terrestris.shogun2.dao.WpsPluginDao;
+import de.terrestris.shogun2.model.wps.WpsPlugin;
+import de.terrestris.shogun2.service.WpsPluginService;
+
+/**
+ * @author Nils Bühner
+ *
+ */
+@RestController
+@RequestMapping("/wpsplugins")
+public class WpsPluginRestController<E extends WpsPlugin, D extends WpsPluginDao<E>, S extends WpsPluginService<E, D>>
+		extends PluginRestController<E, D, S> {
+
+	/**
+	 * Default constructor, which calls the type-constructor
+	 */
+	@SuppressWarnings("unchecked")
+	public WpsPluginRestController() {
+		this((Class<E>) WpsPlugin.class);
+	}
+
+	/**
+	 * Constructor that sets the concrete entity class for the controller.
+	 * Subclasses MUST call this constructor.
+	 */
+	protected WpsPluginRestController(Class<E> entityClass) {
+		super(entityClass);
+	}
+
+	/**
+	 * We have to use {@link Qualifier} to define the correct service here.
+	 * Otherwise, spring can not decide which service has to be autowired here
+	 * as there are multiple candidates.
+	 */
+	@Override
+	@Autowired
+	@Qualifier("wpsPluginService")
+	public void setService(S service) {
+		this.service = service;
+	}
+}
---
+++ src/shogun2-core/src/main/java/de/terrestris/shogun2/service/WpsPluginService.java
@@ -0,0 +1,48 @@
+package de.terrestris.shogun2.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import de.terrestris.shogun2.dao.WpsPluginDao;
+import de.terrestris.shogun2.model.wps.WpsPlugin;
+
+/**
+ * Service class for the {@link WpsPlugin} model.
+ *
+ * @author Nils Bühner
+ * @see AbstractCrudService
+ *
+ */
+@Service("wpsPluginService")
+public class WpsPluginService<E extends WpsPlugin, D extends WpsPluginDao<E>> extends
+		PluginService<E, D> {
+
+	/**
+	 * Default constructor, which calls the type-constructor
+	 */
+	@SuppressWarnings("unchecked")
+	public WpsPluginService() {
+		this((Class<E>) WpsPlugin.class);
+	}
+
+	/**
+	 * Constructor that sets the concrete entity class for the service.
+	 * Subclasses MUST call this constructor.
+	 */
+	protected WpsPluginService(Class<E> entityClass) {
+		super(entityClass);
+	}
+
+	/**
+	 * We have to use {@link Qualifier} to define the correct dao here.
+	 * Otherwise, spring can not decide which dao has to be autowired here
+	 * as there are multiple candidates.
+	 */
+	@Override
+	@Autowired
+	@Qualifier("wpsPluginDao")
+	public void setDao(D dao) {
+		this.dao = dao;
+	}
+}
