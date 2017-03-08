e58ee9631bf03d12ad8e3dbafa241a0249309b52
Daniel Koch <koch@terrestris.de>
2017-03-08 09:35:26 +0100

    Unlink WpsProcessExecute entities from any associated WpsPlugin on deletion

--- src/shogun2-core/src/main/java/de/terrestris/shogun2/service/WpsProcessExecuteService.java
+++ src/shogun2-core/src/main/java/de/terrestris/shogun2/service/WpsProcessExecuteService.java
@@ -1,10 +1,17 @@
 package de.terrestris.shogun2.service;
 
+import java.util.List;
+
+import org.hibernate.criterion.Restrictions;
+import org.hibernate.criterion.SimpleExpression;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 
+import de.terrestris.shogun2.dao.WpsPluginDao;
 import de.terrestris.shogun2.dao.WpsProcessExecuteDao;
+import de.terrestris.shogun2.model.wps.WpsPlugin;
 import de.terrestris.shogun2.model.wps.WpsProcessExecute;
 
 /**
@@ -17,6 +24,13 @@ import de.terrestris.shogun2.model.wps.WpsProcessExecute;
 @Service("wpsProcessExecuteService")
 public class WpsProcessExecuteService<E extends WpsProcessExecute, D extends WpsProcessExecuteDao<E>> extends
 		WpsReferenceService<E, D> {
+
+	/**
+	 * The WpsPluginService which we e.g. need when WpsProcessExecutes are deleted.
+	 */
+	@Autowired
+	@Qualifier("wpsPluginService")
+	private WpsPluginService<WpsPlugin, WpsPluginDao<WpsPlugin>> wpsPluginService;
 
 	/**
 	 * Default constructor, which calls the type-constructor
@@ -35,6 +49,45 @@ public class WpsProcessExecuteService<E extends WpsProcessExecute, D extends Wps
 	}
 
 	/**
+	 * Removes the passed WpsProcessExecute from all WpsPlugins and afterwards
+	 * deletes the WpsProcessExecute itself. This overrides the generic method
+	 * to delete {@link AbstractCrudService#delete(de.terrestris.shogun2.model.PersistentObject)}.
+	 *
+	 * @param wpsProcessExecute
+	 */
+	@Override
+	@PreAuthorize("hasRole(@configHolder.getSuperAdminRoleName()) or hasPermission(#plugin, 'DELETE')")
+	public void delete(E wpsProcessExecute) {
+		if (wpsPluginService == null) {
+			LOG.error("WPSProcessExecute cannot be deleted, failed to autowire WpsPluginService");
+			return;
+		}
+
+		WpsPluginDao<WpsPlugin> wpsPluginDao = wpsPluginService.getDao();
+
+		SimpleExpression eqProcess = Restrictions.eq("process", wpsProcessExecute);
+		List<WpsPlugin> wpsPlugins = wpsPluginDao.findByCriteria(eqProcess);
+
+		Integer processId = wpsProcessExecute.getId();
+		for (WpsPlugin wpsPlugin : wpsPlugins) {
+			WpsProcessExecute process = wpsPlugin.getProcess();
+			if (process != null) {
+				String msg = String.format(
+					"Remove WpsProcessExecute (id=%s) from WpsPlugin (id=%s)",
+					processId, wpsPlugin.getId()
+				);
+				LOG.debug(msg);
+				wpsPlugin.setProcess(null);
+				wpsPluginService.saveOrUpdate(wpsPlugin);
+			}
+		}
+		LOG.debug(String.format("Delete plugin (id=%s)", processId));
+
+		// Call overridden parent to actually delete the entity itself
+		super.delete(wpsProcessExecute);
+	}
+
+	/**
 	 * We have to use {@link Qualifier} to define the correct dao here.
 	 * Otherwise, spring can not decide which dao has to be autowired here
 	 * as there are multiple candidates.
@@ -44,5 +97,12 @@ public class WpsProcessExecuteService<E extends WpsProcessExecute, D extends Wps
 	@Qualifier("wpsProcessExecuteDao")
 	public void setDao(D dao) {
 		this.dao = dao;
+	}
+
+	/**
+	 * @return the wpsPluginService
+	 */
+	public WpsPluginService<WpsPlugin, WpsPluginDao<WpsPlugin>> getWpsPluginService() {
+		return wpsPluginService;
 	}
 }
