394e75e787c7bda5e99eb30d1a3a038ba1b7c310
Marc Jansen <jansen@terrestris.de>
2018-02-05 14:08:41 +0100

    Give 500 error also a description

--- src/shogun2-core/src/main/java/de/terrestris/shogun2/service/HttpProxyService.java
+++ src/shogun2-core/src/main/java/de/terrestris/shogun2/service/HttpProxyService.java
@@ -67,7 +67,8 @@ public class HttpProxyService {
 	public static final String ERR_MSG_405 = "ERROR 405: (Method Not Allowed):"
 			+ " The HttpProxyService does not support this request method.";
 
-	public static final String ERR_MSG_500 = "ERROR 500 (Internal Error)";
+	public static final String ERR_MSG_500 = "ERROR 500 (Internal Error)"
+			+ "An internal error occured which prevented further processing.";
 
 	public static final String ERR_MSG_502 = "ERROR 502 (Bad Gateway):"
 			+ " The HttpProxyService does not allow you to access that location.";
