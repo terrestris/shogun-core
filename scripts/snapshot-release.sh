ff51e0027cd731063cc858b87d0dbca2e15c2f00
Nils Bühner <buehner@terrestris.de>
2018-05-16 10:45:46 +0200

    Add quotes to variables in script to avoid errors (#319)

--- scripts/snapshot-release.sh
+++ scripts/snapshot-release.sh
@@ -3,22 +3,22 @@
 # stop at first command failure
 set -e
 
-if [ $TRAVIS != "true" ]; then
+if [ "$TRAVIS" != "true" ]; then
     echo "This script is supposed to be run inside the travis environment."
     exit 1
 fi
 
-if [ $TRAVIS_PULL_REQUEST != "false" ]; then
+if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
     # only proceed if we were called by a merge (and not by a PR request)
     exit 1
 fi
 
-if [ $TRAVIS_BRANCH != "master" ]; then
+if [ "$TRAVIS_BRANCH" != "master" ]; then
     # only proceed if the target branch is master
     exit 1
 fi
 
-if [ $TRAVIS_JDK_VERSION != "oraclejdk8" ]; then
+if [ "$TRAVIS_JDK_VERSION" != "oraclejdk8" ]; then
     # only proceed if the target JDK is oraclejdk8
     exit 1
 fi
