1a03422d19c48b5daf76074718ef85f22a3eeccf
Nils Bühner <buehner@terrestris.de>
2018-12-07 15:41:49 +0100

    Intercept OGC requests with basic auth by default

---
+++ src/shogun2-core/src/main/java/de/terrestris/shogun2/util/interceptor/impl/WfsRequestInterceptor.java
@@ -0,0 +1,86 @@
+/**
+ *
+ */
+package de.terrestris.shogun2.util.interceptor.impl;
+
+import static org.apache.logging.log4j.LogManager.getLogger;
+
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Value;
+
+import de.terrestris.shogun2.util.interceptor.BasicAuthHeaderRequest;
+import de.terrestris.shogun2.util.interceptor.MutableHttpServletRequest;
+import de.terrestris.shogun2.util.interceptor.WfsRequestInterceptorInterface;
+
+/**
+ * Interceptor class for WFS requests.
+ * Adds basic auth headers based on the GS properties by default.
+ *
+ * @author Nils Bühner
+ *
+ */
+public class WfsRequestInterceptor implements WfsRequestInterceptorInterface {
+
+	/**
+	 *
+	 */
+	private static final Logger LOG = getLogger(WfsRequestInterceptor.class);
+
+	/**
+	 *
+	 */
+	@Value("${geoserver.username}")
+	private String gsUser;
+
+	/**
+	 *
+	 */
+	@Value("${geoserver.password}")
+	private String gsPass;
+
+    /**
+     *
+     */
+	@Override
+	public MutableHttpServletRequest interceptGetCapabilities(MutableHttpServletRequest request) {
+		LOG.debug("Intercepting WFS GetCapabilities and adding Basic auth credentials.");
+		return new BasicAuthHeaderRequest(request, gsUser, gsPass);
+	}
+
+    /**
+     *
+     */
+	@Override
+	public MutableHttpServletRequest interceptDescribeFeatureType(MutableHttpServletRequest request) {
+		LOG.debug("Intercepting WFS DescribeFeatureType and adding Basic auth credentials.");
+		return new BasicAuthHeaderRequest(request, gsUser, gsPass);
+	}
+
+    /**
+     *
+     */
+	@Override
+	public MutableHttpServletRequest interceptGetFeature(MutableHttpServletRequest request) {
+		LOG.debug("Intercepting WFS GetFeature and adding Basic auth credentials.");
+		return new BasicAuthHeaderRequest(request, gsUser, gsPass);
+	}
+
+    /**
+     *
+     */
+	@Override
+	public MutableHttpServletRequest interceptLockFeature(MutableHttpServletRequest request) {
+		LOG.debug("Intercepting WFS LockFeature and adding Basic auth credentials.");
+		return new BasicAuthHeaderRequest(request, gsUser, gsPass);
+	}
+
+    /**
+     *
+     */
+	@Override
+	public MutableHttpServletRequest interceptTransaction(MutableHttpServletRequest request) {
+		LOG.debug("Intercepting WFS Transaction and adding Basic auth credentials.");
+		return new BasicAuthHeaderRequest(request, gsUser, gsPass);
+	}
+
+}
---
+++ src/shogun2-core/src/main/java/de/terrestris/shogun2/util/interceptor/impl/WpsRequestInterceptor.java
@@ -0,0 +1,68 @@
+/**
+ *
+ */
+package de.terrestris.shogun2.util.interceptor.impl;
+
+import static org.apache.logging.log4j.LogManager.getLogger;
+
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Value;
+
+import de.terrestris.shogun2.util.interceptor.BasicAuthHeaderRequest;
+import de.terrestris.shogun2.util.interceptor.MutableHttpServletRequest;
+import de.terrestris.shogun2.util.interceptor.WpsRequestInterceptorInterface;
+
+/**
+ * Interceptor class for WPS requests.
+ * Adds basic auth headers based on the GS properties by default.
+ *
+ * @author Nils Bühner
+ *
+ */
+public class WpsRequestInterceptor implements WpsRequestInterceptorInterface {
+
+	/**
+	 *
+	 */
+	private static final Logger LOG = getLogger(WpsRequestInterceptor.class);
+
+	/**
+	 *
+	 */
+	@Value("${geoserver.username}")
+	private String gsUser;
+
+	/**
+	 *
+	 */
+	@Value("${geoserver.password}")
+	private String gsPass;
+
+	/**
+	 * @see de.terrestris.shogun2.util.interceptor.WpsRequestInterceptorInterface#interceptGetCapabilities(de.terrestris.shogun2.util.interceptor.MutableHttpServletRequest)
+	 */
+	@Override
+	public MutableHttpServletRequest interceptGetCapabilities(MutableHttpServletRequest request) {
+		LOG.debug("Intercepting WPS GetCapabilities and adding Basic auth credentials.");
+		return new BasicAuthHeaderRequest(request, gsUser, gsPass);
+	}
+
+	/**
+	 * @see de.terrestris.shogun2.util.interceptor.WpsRequestInterceptorInterface#interceptDescribeProcess(de.terrestris.shogun2.util.interceptor.MutableHttpServletRequest)
+	 */
+	@Override
+	public MutableHttpServletRequest interceptDescribeProcess(MutableHttpServletRequest request) {
+		LOG.debug("Intercepting WPS DescribeProcess and adding Basic auth credentials.");
+		return new BasicAuthHeaderRequest(request, gsUser, gsPass);
+	}
+
+	/**
+	 * @see de.terrestris.shogun2.util.interceptor.WpsRequestInterceptorInterface#interceptExecute(de.terrestris.shogun2.util.interceptor.MutableHttpServletRequest)
+	 */
+	@Override
+	public MutableHttpServletRequest interceptExecute(MutableHttpServletRequest request) {
+		LOG.debug("Intercepting WPS Execute and adding Basic auth credentials.");
+		return new BasicAuthHeaderRequest(request, gsUser, gsPass);
+	}
+
+}
