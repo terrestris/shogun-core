220974c038442d90cfb7f15b5cc440bce01eb43d
Nils Bühner <buehner@terrestris.de>
2019-05-17 10:07:29 +0200

    Add missing getters and setters

--- src/shogun-core-main/src/main/java/de/terrestris/shoguncore/importer/communication/RESTDataFile.java
+++ src/shogun-core-main/src/main/java/de/terrestris/shoguncore/importer/communication/RESTDataFile.java
@@ -11,37 +11,115 @@ public class RESTDataFile extends RESTData {
     /**
      *
      */
-    @SuppressWarnings("unused")
     private String format;
 
     /**
      *
      */
-    @SuppressWarnings("unused")
     private String location;
 
     /**
      *
      */
-    @SuppressWarnings("unused")
     private String file;
 
     /**
      *
      */
-    @SuppressWarnings("unused")
     private String href;
 
     /**
      *
      */
-    @SuppressWarnings("unused")
     private String prj;
 
     /**
      *
      */
-    @SuppressWarnings("unused")
     private List<String> other;
+
+    /**
+     * @return the format
+     */
+    public String getFormat() {
+        return format;
+    }
+
+    /**
+     * @param format the format to set
+     */
+    public void setFormat(String format) {
+        this.format = format;
+    }
+
+    /**
+     * @return the location
+     */
+    public String getLocation() {
+        return location;
+    }
+
+    /**
+     * @param location the location to set
+     */
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    /**
+     * @return the file
+     */
+    public String getFile() {
+        return file;
+    }
+
+    /**
+     * @param file the file to set
+     */
+    public void setFile(String file) {
+        this.file = file;
+    }
+
+    /**
+     * @return the href
+     */
+    public String getHref() {
+        return href;
+    }
+
+    /**
+     * @param href the href to set
+     */
+    public void setHref(String href) {
+        this.href = href;
+    }
+
+    /**
+     * @return the prj
+     */
+    public String getPrj() {
+        return prj;
+    }
+
+    /**
+     * @param prj the prj to set
+     */
+    public void setPrj(String prj) {
+        this.prj = prj;
+    }
+
+    /**
+     * @return the other
+     */
+    public List<String> getOther() {
+        return other;
+    }
+
+    /**
+     * @param other the other to set
+     */
+    public void setOther(List<String> other) {
+        this.other = other;
+    }
 
 }
--- src/shogun-core-main/src/main/java/de/terrestris/shoguncore/importer/transform/RESTDateFormatTransform.java
+++ src/shogun-core-main/src/main/java/de/terrestris/shoguncore/importer/transform/RESTDateFormatTransform.java
@@ -9,19 +9,58 @@ public class RESTDateFormatTransform extends RESTTransform {
     /**
      *
      */
-    @SuppressWarnings("unused")
     private String href;
 
     /**
      *
      */
-    @SuppressWarnings("unused")
     private String field;
 
     /**
      *
      */
-    @SuppressWarnings("unused")
     private String format;
+
+    /**
+     * @return the href
+     */
+    public String getHref() {
+        return href;
+    }
+
+    /**
+     * @param href the href to set
+     */
+    public void setHref(String href) {
+        this.href = href;
+    }
+
+    /**
+     * @return the field
+     */
+    public String getField() {
+        return field;
+    }
+
+    /**
+     * @param field the field to set
+     */
+    public void setField(String field) {
+        this.field = field;
+    }
+
+    /**
+     * @return the format
+     */
+    public String getFormat() {
+        return format;
+    }
+
+    /**
+     * @param format the format to set
+     */
+    public void setFormat(String format) {
+        this.format = format;
+    }
 
 }
