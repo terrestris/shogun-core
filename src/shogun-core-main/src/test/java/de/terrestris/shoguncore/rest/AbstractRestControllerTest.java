07ff506390e8aa62b9de197fa01a4043cc1d7562
Andre Henn <henn@terrestris.de>
2020-01-22 10:59:15 +0100

    organize imports

--- src/shogun-core-main/src/test/java/de/terrestris/shoguncore/rest/AbstractRestControllerTest.java
+++ src/shogun-core-main/src/test/java/de/terrestris/shoguncore/rest/AbstractRestControllerTest.java
@@ -1,26 +1,13 @@
 package de.terrestris.shoguncore.rest;
 
-import static org.hamcrest.Matchers.hasSize;
-import static org.hamcrest.Matchers.is;
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.same;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.doThrow;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyNoMoreInteractions;
-import static org.mockito.Mockito.when;
-import static org.mockito.Mockito.doAnswer;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import java.util.Arrays;
-
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import de.terrestris.shoguncore.dao.GenericHibernateDao;
+import de.terrestris.shoguncore.helper.IdHelper;
+import de.terrestris.shoguncore.model.PersistentObject;
+import de.terrestris.shoguncore.service.AbstractCrudService;
+import de.terrestris.shoguncore.util.json.ShogunCoreJsonObjectMapper;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
@@ -36,15 +23,15 @@ import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.Arrays;
 
-import de.terrestris.shoguncore.dao.GenericHibernateDao;
-import de.terrestris.shoguncore.helper.IdHelper;
-import de.terrestris.shoguncore.model.PersistentObject;
-import de.terrestris.shoguncore.service.AbstractCrudService;
-import de.terrestris.shoguncore.util.json.ShogunCoreJsonObjectMapper;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.same;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 /**
  * @author Kai Volland
