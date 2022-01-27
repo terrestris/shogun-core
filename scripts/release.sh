665d60b00dc7650494f7d6e374db589dc2edd16a
Andreas Schmitz <schmitz@terrestris.de>
2022-01-27 12:06:14 +0100

    Update scm plugin version, debug release failure

--- scripts/release.sh
+++ scripts/release.sh
@@ -19,7 +19,7 @@ fi
 
 # check if the input parameter DEVELOPMENT_VERSION is valid
 DEVELOPMENT_VERSION="$2"
-if [[ ! $DEVELOPMENT_VERSION =~ ^([0-9]+\.[0-9]+\.[0-9])(\-SNAPSHOT)$ ]]; then
+if [[ ! $DEVELOPMENT_VERSION =~ ^([0-9]+\.[0-9]+\.[0-9]+)(\-SNAPSHOT)$ ]]; then
     echo "Error: DEVELOPMENT_VERSION must be in X.Y.Z-SNAPSHOT format, but was $DEVELOPMENT_VERSION"
     exit 1
 fi
@@ -29,7 +29,7 @@ SCRIPTDIR=`dirname "$0"`
 pushd $SCRIPTDIR/../src/
 
 mvn release:clean
-mvn release:prepare --batch-mode -DreleaseVersion=$RELEASE_VERSION -DdevelopmentVersion=$DEVELOPMENT_VERSION -Darguments="-DskipTests"
-mvn release:perform --batch-mode
+mvn release:prepare -X --batch-mode -DreleaseVersion=$RELEASE_VERSION -DdevelopmentVersion=$DEVELOPMENT_VERSION -Darguments="-DskipTests"
+mvn release:perform -X --batch-mode
 
 popd
