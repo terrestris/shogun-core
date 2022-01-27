285f9a47769ea2f03c4ef7cc9a78b22c996fd0dc
Andreas Schmitz <schmitz@terrestris.de>
2022-01-27 11:30:19 +0100

    Allow multi digit patch releases (#423)

--- scripts/release.sh
+++ scripts/release.sh
@@ -12,14 +12,14 @@ fi
 
 # check if the input parameter RELEASE_VERSION is valid
 RELEASE_VERSION="$1"
-if [[ ! $RELEASE_VERSION =~ ^([0-9]+\.[0-9]+\.[0-9])$ ]]; then
+if [[ ! $RELEASE_VERSION =~ ^([0-9]+\.[0-9]+\.[0-9]+)$ ]]; then
     echo "Error: RELEASE_VERSION must be in X.Y.Z format, but was $RELEASE_VERSION"
     exit 1
 fi
 
 # check if the input parameter DEVELOPMENT_VERSION is valid
 DEVELOPMENT_VERSION="$2"
-if [[ ! $DEVELOPMENT_VERSION =~ ^([0-9]+\.[0-9]+\.[0-9])(\-SNAPSHOT)$ ]]; then
+if [[ ! $DEVELOPMENT_VERSION =~ ^([0-9]+\.[0-9]+\.[0-9]+)(\-SNAPSHOT)$ ]]; then
     echo "Error: DEVELOPMENT_VERSION must be in X.Y.Z-SNAPSHOT format, but was $DEVELOPMENT_VERSION"
     exit 1
 fi
